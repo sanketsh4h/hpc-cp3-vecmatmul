745dc94bc92f17aba03c08e94aedb854404a526b
sanketsh4h <sanketshah5135@gmail.com>
2023-10-06 20:59:21 -0700

    First attempt

--- benchmark.cpp
+++ benchmark.cpp
@@ -93,12 +93,17 @@ int main(int argc, char** argv)
         memcpy((void *)Ycopy, (const void *)Y, sizeof(double)*n);
 
         // insert start timer code here
+        auto start_time = std::chrono::high_resolution_clock::now();
 
-        // call the method to do the work
-        my_dgemv(n, A, X, Y); 
+        // Call the method to do the work
+        my_dgemv(n, A, X, Y);
 
-        // insert end timer code here, and print out the elapsed time for this problem size
+        // End timer
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
+        // Print out the elapsed time for this problem size
+        std::cout << "Elapsed time for N=" << n << ": " << duration.count() << " milliseconds" << std::endl;
 
         // now invoke the cblas method to compute the matrix-vector multiplye
         reference_dgemv(n, Acopy, Xcopy, Ycopy);
--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -7,5 +7,11 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of basic matrix multiply
+    for (int i = 0; i < n; i++) {
+        y[i] = 0.0;  // Initialize the y vector with zeros
+        for (int j = 0; j < n; j++) {
+            y[i] += A[i * n + j] * x[j];  // Matrix-vector multiplication
+        }
+    }
 }
+
