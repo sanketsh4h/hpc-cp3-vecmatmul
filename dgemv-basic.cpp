f02a8cc2b2fd711658e6f65332f9a3e039b3f2a6
sanketsh4h <sanketshah5135@gmail.com>
2023-12-24 20:52:33 -0800

    Final update

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,7 +8,7 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
     for (int i = 0; i < n; i++) {
-        y[i] = 0.0;  // Initialize the y vector with zeros
+        // y[i] = 0.0;  // Initialize the y vector with zeros
         for (int j = 0; j < n; j++) {
             y[i] += A[i * n + j] * x[j];  // Matrix-vector multiplication
         }
